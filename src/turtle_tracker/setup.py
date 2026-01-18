51319ae7cacee1c7d858d44a9b6f20d1c0d70729
ZhiHui <145458582+ZhiHui05@users.noreply.github.com>
2026-01-19 00:03:06 +0100

    18/1

--- src/turtle_tracker/setup.py
+++ src/turtle_tracker/setup.py
@@ -12,19 +12,24 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Copiar los archivos srv
+        (os.path.join('share', package_name, 'srv'), glob('srv/*.srv')),
+        # Copiar los archivos launch
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.xml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='zli14z',
     maintainer_email='zli14z@todo.todo',
-    description='Turtle Tracker con Service E3 E4 y Action E6',
-    license='Apache-2.0',
+    description='TODO: Package description',
+    license='TODO: License declaration',
     extras_require={
-        'test': ['pytest'],
+        'test': [
+            'pytest',
+        ],
     },
     entry_points={
-        'console_scripts':  [
+        'console_scripts': [
             'explorer_service_node = turtle_tracker.explorer_service_node:main',
             'service_client_node = turtle_tracker.service_client_node:main',
             'explorer_action_node = turtle_tracker.explorer_action_node:main',
@@ -32,3 +37,4 @@ setup(
         ],
     },
 )
+
